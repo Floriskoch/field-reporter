03336ff1c3c0fae4fd544172c5501884ee978bce
Floris S. Koch <floris@bolden.nl>
2017-01-17 21:18:11 +0100

    CTA adjustments

--- site/snippets/home.cta.php
+++ site/snippets/home.cta.php
@@ -28,19 +28,40 @@ if (isset($_POST['ctaEmail'])) {
             <h3 class="mt0 mb2"><?php echo $data->heading()->text() ?></h3>
             <p><?php echo $data->text()->kirbytext() ?></p>
 
-            <?php if (!isset($response)) { ?>
-              <form action="/#section-demo" method="post" class="md-flex items-center">
+              <!-- Begin MailChimp Signup Form -->
+              <div id="mc_embed_signup">
+                <form action="//fieldreporter.us14.list-manage.com/subscribe/post?u=147fcc0304b7f2bac397eba42&amp;id=36025566f5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll">
+                    <div class="mc-field-group">
+                      <label for="mce-EMAIL" class="mb1 inline-block">Email address
+                      </label>
+                      <input type="email" value="" name="EMAIL" class="required email input-cta block col-12 mb2" id="mce-EMAIL">
+                    </div>
+                    <div class="mc-field-group">
+                      <label for="mce-MMERGE1" class="mb1 inline-block">Name
+                      </label>
+                      <input type="text" value="" name="MMERGE1" class="required input-cta block col-12 mb2" id="mce-MMERGE1">
+                    </div>
+                    <div class="mc-field-group">
+                      <label for="mce-MMERGE2" class="mb1 inline-block">Organisation
+                      </label>
+                      <input type="text" value="" name="MMERGE2" class="required input-cta block col-12 mb2" id="mce-MMERGE2">
+                    </div>
+                    <div class="mc-field-group size1of2">
+                      <label for="mce-MMERGE3" class="mb1 inline-block">Phone (not required) </label>
+                      <input type="text" name="MMERGE3" class="input-cta block col-12 mb3" value="" id="mce-MMERGE3">
+                    </div>
+                    <div id="mce-responses" class="clear">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                      <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_147fcc0304b7f2bac397eba42_36025566f5" tabindex="-1" value=""></div>
+                    <div class="clear"><input type="submit" value="<?php echo $data->button()->text() ?>" name="subscribe" id="mc-embedded-subscribe" class="btn btn--cta bold col-12 lg-"></div>
+                  </div>
+                </form>
+              </div>
 
-                <div class="input-cta js-input-cta sm-flex">
-                  <input type="email" class="flex-auto mb2 sm-mb0" placeholder="<?php echo $data->input()->text() ?>" name="ctaEmail">
-                  <button type="submit" class="sm-ml2 btn btn--cta" disabled><?php echo $data->button()->text() ?></button>
-                </div>
-
-
-              </form>
-            <?php } else { ?>
-              <p><?php echo $data->thankyou()->text() ?></p>
-            <?php } ?>
+              <!--End mc_embed_signup-->
 
           </div>
 
